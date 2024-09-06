0f75ca604e13a06bc8fece78c614abad33784a57
AkaleshTanwer <akalesh.tanwer@gravityer.com>
2024-09-06 10:29:22 +0530

    Updated Skills and resume

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 const downloadButtons = document.querySelectorAll('.download-cv-btn');
 
 // Attach the click event listener to each button
-downloadButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
+downloadButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
     // Set the path to your CV file in the "documents" folder
-    var cvFilePath = 'documents/AkaleshT_Full_Stack_2.8yrs.docx';
+    var cvFilePath = 'documents/AkaleshT_Full_Stack_3yrs.docx';
 
     // Create a temporary anchor element
     var link = document.createElement('a');
@@ -14,7 +14,7 @@ downloadButtons.forEach(function(button) {
     link.href = cvFilePath;
 
     // Set the download attribute to specify the filename when the user downloads the file
-    link.download = 'AkaleshT_Full_Stack_2.8yrs.docx';
+    link.download = 'AkaleshT_Full_Stack_3yrs.docx';
 
     // Append the link to the document body
     document.body.appendChild(link);
